f481bd277424edc3458913ddc2491781bd15349b
ftolentino <filmer.tolentino@gmail.com>
2022-09-09 15:39:04 -0700

    Add NewCoffeeForm componenet

---
+++ src/components/NewCoffeeForm.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { v4 } from "uuid";
+import PropTypes from "prop-types";
+import ReusableForm from "./ReusableForm";
+
+function NewCoffeeForm(props) {
+  function handleNewCoffeeFormSubmission(event) {
+    event.preventDefault();
+    props.onNewCoffeeCreation({
+      name: event.target.name.value,
+      origin: event.target.origin.value,
+      price: event.target.price.value,
+      roast: event.target.roast.value,
+      id: v4(),
+    });
+  }
+
+  return (
+    <React.Fragment>
+      <ReusableForm
+        formSubmissionHandler={handleNewCoffeeFormSubmission}
+        buttonText="Help!" />
+    </React.Fragment>
+  );
+}
+
+NewCoffeeForm.propTypes = {
+  onNewCoffeeCreation: PropTypes.func,
+};
+
+export default NewCoffeeForm;
