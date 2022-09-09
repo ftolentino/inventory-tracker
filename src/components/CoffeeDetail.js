bde495bf8d84e3db837013acbfc1f2b0e759021d
ftolentino <filmer.tolentino@gmail.com>
2022-09-09 15:39:24 -0700

    Add CoffeeDetail component

---
+++ src/components/CoffeeDetail.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from 'prop-types';
+
+function CoffeeDetail(props){
+  const { coffee, onClickingDelete } = props;
+  
+  return (
+    <React.Fragment>
+      <h1>Coffee Detail</h1>
+      <h3>{coffee.origin} - {coffee.name}</h3>
+      <p><em>{coffee.roast}</em></p>
+      <p><em>${props.price}</em></p>
+      <button onClick={ props.onClickingEdit }>Update Coffee</button>
+      <button onClick={()=> onClickingDelete(coffee.id)}>Delete Coffee</button>
+      <hr/>
+    </React.Fragment>
+  );
+}
+
+CoffeeDetail.propTypes = {
+  coffee: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
+
+export default CoffeeDetail;
