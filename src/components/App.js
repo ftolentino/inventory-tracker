f0d48f8a0dcf8ad4e1162ea365d0eb29a7ebb0e6
ftolentino <filmer.tolentino@gmail.com>
2022-09-09 13:57:51 -0700

    Update App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../App.css';
 
+import Header from './Header';
+
 function App() {
   return (
     <React.Fragment>
-      <h1>hello</h1>
+      <Header />
     </React.Fragment>
   );
 }
