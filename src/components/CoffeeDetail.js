b6e985239595d28725c1d74214534cf06423c331
ftolentino <filmer.tolentino@gmail.com>
2022-09-11 00:39:35 -0700

    Update JSX, add onClickSellBag prop

--- src/components/CoffeeDetail.js
+++ src/components/CoffeeDetail.js
@@ -2,16 +2,18 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 function CoffeeDetail(props){
-  const { coffee, onClickingDelete } = props;
+  const { coffee, onClickingDelete, onClickSellBag } = props;
   
   return (
     <React.Fragment>
       <h1>Coffee Detail</h1>
-      <h3>{coffee.origin} - {coffee.name}</h3>
-      <p><em>{coffee.roast}</em></p>
-      <p><em>${props.price}</em></p>
+      <h3>{coffee.name} - Origin: {coffee.origin}</h3>
+      <p><em>Roast: {coffee.roast}</em></p>
+      <p><em>${coffee.price}/lb</em></p>
+      <p><em>Beans: {coffee.poundsOfBeans}/lbs</em></p>
       <button onClick={ props.onClickingEdit }>Update Coffee</button>
       <button onClick={()=> onClickingDelete(coffee.id)}>Delete Coffee</button>
+      <button onClick={()=> onClickSellBag()}>Sell 1lb</button>
       <hr/>
     </React.Fragment>
   );
@@ -20,7 +22,8 @@ function CoffeeDetail(props){
 CoffeeDetail.propTypes = {
   coffee: PropTypes.object,
   onClickingDelete: PropTypes.func,
-  onClickingEdit: PropTypes.func
+  onClickingEdit: PropTypes.func,
+  onClickSellBag: PropTypes.func
 };
 
 export default CoffeeDetail;
